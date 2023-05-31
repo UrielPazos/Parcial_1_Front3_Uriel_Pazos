2c87fd8bcc81b76d21396dc1bcb59dbee7648644
upazos <upazos@mobile-i-business.com>
2023-05-31 00:50:49 -0300

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./Components/Card";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+}
+
+
+export default App;
